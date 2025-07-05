127a8c7b1428ff37eab22c18ac3dc0af5682a090
John Sturgeon <john@sturgeon.me>
2025-07-04 21:01:56 -0700

    Added basic authentication

--- app/api/secrets.py
+++ app/api/secrets.py
@@ -1,15 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.services import crypto
-
+from app.core.config import Config
 
 router = APIRouter()
+security = HTTPBearer()
+config = Config.get_config()
+
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials.credentials != config.API_AUTH_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or missing token"
+        )
 
 
 @router.get("/encrypt/{plain_text}")
-def encrypt_secret(plain_text: str):
+def encrypt_secret(
+    plain_text: str, _: HTTPAuthorizationCredentials = Depends(verify_token)
+):
     return {"encrypted": crypto.encrypt(plain_text)}
 
 
 @router.get("/decrypt/{encrypted_text}")
-def decrypt_secret(encrypted_text: str):
+def decrypt_secret(
+    encrypted_text: str,
+    _: HTTPAuthorizationCredentials = Depends(verify_token),
+):
     return {"decrypted": crypto.decrypt(encrypted_text)}
--- app/core/config.py
+++ app/core/config.py
@@ -12,11 +12,13 @@ class Config:
 
     # pylint: disable=invalid-name
     FERNET_KEY: str
+    API_AUTH_TOKEN: str
 
     @classmethod
     def get_config(cls):
         """Factory method for returning the correct config"""
         config = cls(
             FERNET_KEY=os.getenv("FERNET_KEY"),
+            API_AUTH_TOKEN=os.getenv("API_AUTH_TOKEN"),
         )
         return config
