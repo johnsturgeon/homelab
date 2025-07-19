cafdcf8b46620bbeb65c9860994773d1ce3a0aa7
John Sturgeon <john@sturgeon.me>
2025-07-18 18:40:07 -0700

    Added new API for getting count of playlist

--- app/api/tidal_api.py
+++ app/api/tidal_api.py
@@ -3,7 +3,10 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from app.services.auth import verify_token
 
-from app.services.tidal import delete_songs_from_todo_playlist
+from app.services.tidal import (
+    delete_songs_from_todo_playlist,
+    get_song_count_from_todo_playlist,
+)
 
 router = APIRouter()
 security = HTTPBearer()
@@ -15,3 +18,11 @@ async def delete_songs_from_todo(
 ):
     songs_deleted: int = await delete_songs_from_todo_playlist()
     return {"total_songs_deleted": songs_deleted}
+
+
+@router.get("/count_of_songs_in_todo")
+async def count_of_songs_in_todo(
+    _: HTTPAuthorizationCredentials = Depends(verify_token),
+):
+    songs_in_playlist: int = await get_song_count_from_todo_playlist()
+    return {"total_songs_in_playlist": songs_in_playlist}
--- app/services/tidal.py
+++ app/services/tidal.py
@@ -9,19 +9,36 @@ from app.core.config import global_config
 
 
 async def delete_songs_from_todo_playlist() -> int:
+    todo_playlist = await get_todo_playlists()
+    tracks_deleted: int = 0
+    playlist_ids: List[int] = []
+    for track in todo_playlist.tracks():
+        playlist_ids.append(track.id)
+    tracks_deleted += len(playlist_ids)
+    for playlist_id in playlist_ids:
+        todo_playlist.remove_by_id(str(playlist_id))
+    return tracks_deleted
+
+
+async def get_todo_playlists():
+    await global_config.load_secrets()
     config = tidalapi.Config()
     session = tidalapi.Session(config)
     token_type = "Bearer"
     access_token = global_config.TIDAL_ACCESS_TOKEN
     refresh_token = global_config.TIDAL_REFRESH_TOKEN
     session.load_oauth_session(token_type, access_token, refresh_token)
-    tracks_deleted: int = 0
     for playlist in session.user.playlists():
         if playlist.name == "TODO: Sync To Plex":
-            playlist_ids: List[int] = []
-            for track in playlist.tracks():
-                playlist_ids.append(track.id)
-            tracks_deleted += len(playlist_ids)
-            for playlist_id in playlist_ids:
-                playlist.remove_by_id(str(playlist_id))
-    return tracks_deleted
+            return playlist
+    return None
+
+
+async def get_song_count_from_todo_playlist() -> int:
+    todo_playlist = await get_todo_playlists()
+    return len(todo_playlist.tracks())
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_song_count_from_todo_playlist())
